a96b94f61b5cca397cc854e4b08c329c104ea98c
junxioh <junxioh@uci.edu>
2025-03-10 20:40:29 -0700

    update.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -50,25 +50,19 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     return distance;
 }
 
-vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
     vector<int> path;
     
    
-    if (distances[destination] == INF) {
-        return path;
-    }
-    
-    
     for (int at = destination; at != -1; at = previous[at]) {
         path.push_back(at);
     }
    
-    
+  
     reverse(path.begin(), path.end());
     
     return path;
 }
-
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
         cout << endl;
